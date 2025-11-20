5340fd1b564760308b8954df8ced92b6e6366d89
AAdarve <andres.adarve08@gmail.com>
2025-11-19 23:48:51 -0500

    Feat: Setup custom light color change event

---
+++ Source/DialogueSys/Private/Interfaces/DialogueEventActor.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interfaces/DialogueEventActor.h"
+
+// Add default functionality here for any IDialogueEventActor functions that are not pure virtual.
